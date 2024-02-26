2abe1cb69de36dff7cbe7f4d1d08e7a9d5df8cb6
JavaGrinder <tyk85@yandex.ru>
2024-02-27 00:20:14 +0300

    error correction

--- Lesson_1/RpsGameFormatting.java
+++ Lesson_1/RpsGameFormatting.java
@@ -9,29 +9,47 @@ public class RpsGameFormatting {
 
         // Ход первого игрока
         String name1 = "HEL";
-        String sign1 = rock;
-        System.out.println("Ход "+ name1 + ": ");
-
         Random r = new Random();
         int position1 = r.nextInt(100);
-            if (position1 > 66) {
-                sign1 = paper;
-            } else if (position1 > 33) {
-                sign1 = scissors;
-            }
+        String sign1 = rock;
+
+        if (position1 > 66) {
+            sign1 = paper;
+        } else if (position1 > 33) {
+            sign1 = scissors;
+        }
+        System.out.println("Ход "+ name1 + ": ");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print(rock + "\r");
+            Thread.sleep(100);
+            System.out.print(scissors + "\r");
+            Thread.sleep(100);
+            System.out.print(paper + "\r");
+            Thread.sleep(100);
+        }
         System.out.print(sign1);
 
         // Ход второго игрока
         String name2 = "WALLE";
+        int position2 = r.nextInt(100);
         String sign2 = rock;
+
+        if (position2 > 66) {
+            sign2 = paper;
+        } else if (position2 > 33) {
+            sign2 = scissors;
+        }
         System.out.println("\nХод "+ name2 + ": ");
 
-        int position2 = r.nextInt(100);
-            if (position2 > 66) {
-                sign2 = paper;
-            } else if (position2 > 33) {
-                sign2 = scissors;
-            }
+        for (int i = 0; i < 5; i++) {
+            System.out.print(rock + "\r");
+            Thread.sleep(100);
+            System.out.print(scissors + "\r");
+            Thread.sleep(100);
+            System.out.print (paper + "\r");
+            Thread.sleep(100);
+        }
         System.out.print(sign2);
 
         if (sign1.equals(sign2)) {
