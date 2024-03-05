092795a77a4995463fd8855a51f015e60b651e8b
JavaGrinder <tyk85@yandex.ru>
2024-03-05 23:44:05 +0300

    new file

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    
+    String gender = "Man";
+    String name = "Vasya";
+    int height = 175;
+    float weight = 75.5f;
+    int age = 33;
+
+    void walk () {
+        System.out.println("идти");
+    }
+
+    void sit () {
+        System.out.println("сидеть");
+    }
+
+    void run () {
+        System.out.println("бежать");
+    }
+
+    void talk () {
+        System.out.println("говорить");
+    }
+
+    void learnJava () {
+        System.out.println("учить Java");
+    }
+}
