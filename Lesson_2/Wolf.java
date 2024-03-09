7cb45e7e1bbf99eba0d803ec02bc1b7e38a97c63
JavaGrinder <tyk85@yandex.ru>
2024-03-10 00:49:58 +0300

    new file

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,29 @@
+public class Wolf {
+    
+    String gender;
+    String name;
+    float weight;
+    int age;
+    String coloring;
+
+
+    void walk() {
+        System.out.println("идёт");
+    }
+
+    void sit() {
+        System.out.println("сидит");
+    }
+
+    void run() {
+        System.out.println("бежит");
+    }
+
+    void howl() {
+        System.out.println("воет");
+    }
+
+    void hunt() {
+        System.out.println("охотится");
+    }
+}
