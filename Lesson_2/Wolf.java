81c710d9336feab7904d8ecd0e1617f520ffc4a7
JavaGrinder <tyk85@yandex.ru>
2024-03-11 00:32:27 +0300

    update, error cottection

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,29 +1,72 @@
 public class Wolf {
-    
-    String gender;
-    String name;
-    float weight;
-    int age;
-    String coloring;
 
+    private String gender;
+    private String name;
+    private float weight;
+    private int age;
+    private String coloring;
 
-    void walk() {
+    public String getGender() {
+        return gender;
+    }
+
+    void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    void setAge(int age) {
+        if(age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColoring() {
+        return coloring;
+    }
+
+    void setColoring(String coloring) {
+        this.coloring = coloring;
+    }
+
+    public void walk() {
         System.out.println("идёт");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("сидит");
     }
 
-    void run() {
+    public void run() {
         System.out.println("бежит");
     }
 
-    void howl() {
+    public void howl() {
         System.out.println("воет");
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println("охотится");
     }
 }
