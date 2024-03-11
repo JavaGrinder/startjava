9b28b5cea30c806e2cc1a1690e4250c28899aeb4
JavaGrinder <tyk85@yandex.ru>
2024-03-11 13:47:27 +0300

    error correction 2

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,7 +1,6 @@
 public class WolfTest {
     
     public static void main(String[] args) {
-
         Wolf wolfOne = new Wolf();
         wolfOne.setGender("самец");
         wolfOne.setName("Бобик");
